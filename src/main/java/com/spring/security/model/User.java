d579bf5d9b1bdeeb2b989253f7dfa09b271fbdc4
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-03-15 11:27:14 +0200

    Adding Hibernate And Jpa With Models Closes #9

--- src/main/java/com/spring/security/model/User.java
+++ src/main/java/com/spring/security/model/User.java
@@ -1,15 +1,46 @@
 package com.spring.security.model;
 
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
+@Table(name = "user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
-    private String username;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "age")
     private String age;
+
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "active")
+    private int active; // Represent Email if Activel =>1 else not Active =>0
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id")}
+    )
     private Set<Role> roles;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name="user_authorities",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "authorities_id")}
+    )
     private Set<Authorities> authorities;
 }
